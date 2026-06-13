bed26989df61881438a942b1f7250a31bf59d523
Nothingspecial99 <122666701+Nothingspecial99@users.noreply.github.com>
2026-06-13 11:46:22 +0530

    Remove unnecessary things

--- kuba/adapters.py
+++
@@ -1,8 +0,0 @@
-from allauth.account.adapter import DefaultAccountAdapter
-
-
-class KubaAccountAdapter(DefaultAccountAdapter):
-    def get_login_redirect_url(self, request):
-        if request.user.is_superuser:
-            return '/super-admin/organisations/'
-        return '/organisation-admin/'
--- organisations/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('super-admin/organisations/', views.super_admin_organisations, name='super_admin_organisations'),
-    path('organisation-admin/', views.organisation_admin_dashboard, name='organisation_admin_dashboard'),
-]
