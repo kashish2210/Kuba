db11b19f34886a7f6d33868ff89eab46750d80a6
kashish shrivastav <en.shrivastavkashish@gmail.com>
2026-06-14 08:21:57 +0530

    before deploy

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-# Build script for Render. Migrations are handled by preDeployCommand in render.yaml.
 set -o errexit
 
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
+python manage.py migrate --no-input
